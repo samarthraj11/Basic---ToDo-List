4b740eb70df0a15112971becec0fb7883e65ca3c
Samarth Raj <mailforsamarth@gmail.com>
2022-10-19 12:46:39 +0530

    addtion of new tasks

---
+++ app/src/main/java/com/example/todolistpro/TaskAddition.kt
@@ -0,0 +1,35 @@
+package com.example.todolistpro
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+
+class TaskAddition : AppCompatActivity() {
+
+    lateinit var tick:Button
+    lateinit var inputTask:EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_addition)
+
+        tick = findViewById(R.id.tick)
+        inputTask = findViewById(R.id.editTextTextMultiLine)
+        var count:Int = 0;
+
+        tick.setOnClickListener {
+            count++
+            val intent = Intent(this,MainActivity::class.java)
+            val taskDesc:String = inputTask.text.toString()
+            if(count >= 1)
+            {
+                intent.putExtra("desc",taskDesc)
+                intent.putExtra("count",count)
+            }
+            startActivity(intent)
+        }
+    }
+}
